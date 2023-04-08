6e1a050223e6c8d1f8d13f517c3a4b248de8de3e
George Krotov <zvuk24@gmail.com>
2023-04-08 10:13:30 +0700

    Добавьте файлы проекта.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using APP1.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
+using System.Linq;
+
+namespace APP1.Controllers
+{
+    public class HomeController : Controller
+    {
+        ApplicationContext db;
+
+        public HomeController(ApplicationContext context)
+        {
+            db = context;   
+        }
+
+        public async Task<IActionResult> GetPackages()
+        {
+            if (db.Database.CanConnect())
+            {
+                db.Packages = 
+                return View(await db.Packages.ToListAsync());
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/Package.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace APP1.Models
+{
+    public class Package
+    {
+        public Guid Id { get; set; }
+        public string? Number { get; set; }
+        public int? RegNumber { get; set; }
+        public int? Status { get; set; }
+        public DateTime CreateDate_local { get; set; }
+        public DateTime TransferDate { get; set; }
+        public DateTime RegDate { get; set; }
+        public int? RegistrationNumber { get; set; }
+        public DateTime RegistrationDate { get; set; }
+    }
+
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<Package> Packages { get; set; } = null!;
+        public ApplicationContext(DbContextOptions<ApplicationContext> options)
+            : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Package>().ToTable("Package");
+        }
+    }
+
+    
+}
